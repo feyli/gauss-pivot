6964990e1dec4ca23d28964aa53c0d880a3ee71b
Mathis <44325762+feyli@users.noreply.github.com>
2024-11-25 11:15:52 +0000

    Edit app.py

--- app.py
+++ app.py
@@ -1,11 +1,59 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
 
-from flask import Flask
-app = Flask(__name__)
+## PARTIE 1.1 ##
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+# 1.
+def permutation(S, i_1, i_2) :
+    S[i_1], S[i_2] = S[i_2], S[i_1]
+    return S
+
+# 2.
+def dilatation(S, i, x) :
+    S[i] = [e * x for e in S[i]]
+    return S
+
+# 3.
+def transvection(S, i_1, i_2, x) :
+    a = [e * x for e in S[i_2]]
+    S[i_1] = [S[i_1][i] + a[i] for i in range(len(a))]
+    return S
+
+
+## PARTIE 1.2 ##
+
+# 1.
+def est_membre_gauche_nul(E):
+    return sum(E[:-1]) == 0
+
+print(est_membre_gauche_nul([0,0,0,0,5]))
+
+# 2.
+def ligne_a_simplifier(S):
+    for i in range(len(S)):
+        if est_membre_gauche_nul(S[i]):
+            return i
+    return -1
+            
+
+# 3.
+def simplifier(S):
+    ...
+
+## PARTIE 1.3 ##
+
+# 1.
+def coordonnees_pivot(S,k):
+    ...
+
+# 2.
+def pivoter(S,k):
+    ...
+
+# 3.
+def triangulariser(S):
+    ...
+    
+
+
+
+    
+
